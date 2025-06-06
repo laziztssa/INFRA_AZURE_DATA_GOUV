eaa69078e2a5b33931206478b405a504059cfd5f
laziztssa <166864898+laziztssa@users.noreply.github.com>
2025-06-06 22:23:56 +0200

    Add files via upload

---
+++ Environnements/Dev/variables.tf
@@ -0,0 +1,19 @@
+variable "location" {
+  type = string
+  default = "france central"
+}
+
+variable "project_name" {
+  type = string
+  default = "data_gouv"
+}
+
+variable "subscription_id" {
+  type = string
+  default = ""
+}
+
+variable "environment" {
+  type = string
+  default = "dev"
+}
