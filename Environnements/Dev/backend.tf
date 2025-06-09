5ae2a93b82cde9ebee8cde4a4411e12becf9254c
laziztssa <laziztessa2@gmail.com>
2025-06-09 14:15:13 +0200

    reorgansaition du worskspace

--- Environnements/Dev/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-    backend "azurerm" {
-        resource_group_name  = "rg-terraform-state-datagouv"
-        storage_account_name = "stterraformstatedatagouv"
-        container_name       = "tfstatedatagouv"
-        key                  = "datagouv-dev.terraform.tfstate"
-    }
-}
--- Environnements/Dev/terraform.tfvars
+++
@@ -1,4 +0,0 @@
-location = "France Central"
-project_name = "datagouv"
-subscription_id = "718c4b13-f7d4-42a3-89d8-6cea60e3b692"
-environment  = "dev"
